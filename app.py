045ebe9dd49248f0da1077af88d1eaa853577ad6
Achith Jv <achithjv2003@gmail.com>
2025-07-18 10:11:09 +0530

    Initial commit - Dockerized Flask app

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello from Docker!"
+    return jsonify({
+        "status": "success",
+        "message": "Welcome to Achith JV's Flask Docker App!",
+        "description": "This is a containerized Flask application running inside Docker.",
+        "version": "1.0.0",
+        "developer": "Achith JV"
+    })
+
+@app.route('/about')
+def about():
+    return jsonify({
+        "project": "Flask Docker Starter",
+        "goal": "To demonstrate Dockerized Python Flask apps",
+        "technologies": ["Python", "Flask", "Docker"]
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({
+        "status": "UP"
+    })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0')
